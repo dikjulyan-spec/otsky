d81f5e5a58459b04670b54e7178ff30f6c531553
dikjulyan-spec <dik.julyan@gmail.com>
2026-07-03 01:34:05 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// OTSKY CMS config
-// Isi apiUrl dengan URL Web App Google Apps Script setelah deploy.
-// Contoh: https://script.google.com/macros/s/AKfycbx.../exec
-window.OTSKY_CMS_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbxxsfM8F4dDezzgImjOX-15fIGSchXbYlO2cLMJocAeYWc4u5R8T0-BCzktF5P8SSMcbQ/exec",
-  fallbackContentUrl: "content.json",
-  gsheetLoadTimeoutMs: 12000
-};
