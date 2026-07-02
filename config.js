a38049282e5d3ef917b9b1a69b64ba76dbbfcc90
dikjulyan-spec <dik.julyan@gmail.com>
2026-07-03 01:36:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.OTSKY_CMS_CONFIG = {
   // Isi dengan URL Web App Google Apps Script yang berakhiran /exec
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxxsfM8F4dDezzgImjOX-15fIGSchXbYlO2cLMJocAeYWc4u5R8T0-BCzktF5P8SSMcbQ/exec",
 
   // Dipakai kalau apiUrl belum diisi atau GSheet gagal dibaca
   fallbackContentUrl: "content.json",
